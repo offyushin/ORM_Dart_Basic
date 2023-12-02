94ecb9c66c2e98a268fd0c29c9e577fd297eb5f8
Kim.Yushin <offyushin@naver.com>
2023-12-02 18:03:10 +0900

    print 수정

--- lib/23_12_01/game/cleric.dart
+++ lib/23_12_01/game/cleric.dart
@@ -21,11 +21,14 @@ class Cleric {
   // 성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.
   void selfAid() {
     // 끝나는 조건
+    final beforehp = hp;
+    // final selfAidHp = maxHp
+
     if (mp < 5) {
       print('마나부족');
-      return;
+     // return;
     }
-
+      print("$name은 $beforehp HP 에서 $maxHp HP 가 됐다!");
     mp = mp - 5; // mp -= 5
     hp = maxHp;
   }
@@ -43,11 +46,12 @@ class Cleric {
     mp += recoveryMp;
 
     // 단, 최대 MP 보다 더 회복하는 것은 불가능 하다.
-    if (mp > maxMp) {
+    if  (mp > maxMp) {
       mp = maxMp;
+
     }
-    print('$name 은 $beforeMp에서 $recoveryMp 만큼 회복했다');
-    // 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
+    print('$name 은 $beforeMp에서 $recoveryMp 회복했다\n현재 MP:$mp');
+    //리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
     return beforeMp - mp;
 
   }
@@ -60,13 +64,17 @@ void main(){
   final arthur2 = Cleric('아서스2', hp: 35);
   final arthur3 = Cleric('아서스3');
   // final arthur4 = Cleric();
-  arthur1.selfAid();
   arthur1.pray(3);
+
+  arthur2.selfAid();
   arthur1.selfAid();
+  arthur3.selfAid();
+  /* arthur1.selfAid();
   arthur1.pray(1);
+
   arthur1.selfAid();
   arthur1.pray(5);
 
   arthur2.selfAid();
-  arthur2.pray(1);
+  arthur2.pray(1); */
 }
