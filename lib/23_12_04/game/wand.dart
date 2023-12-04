dbe5ad0c27c17755a6f1124e829cdea27f169924
Kim.Yushin <offyushin@naver.com>
2023-12-04 16:57:10 +0900

    연습문제 10-1~10-2 수정

--- lib/23_12_04/game/wand.dart
+++ lib/23_12_04/game/wand.dart
@@ -1,10 +1,17 @@
 class Wand {
-  String name; // 이름
-  double power; // 마력
+  String _name; // 이름
+  double _power; // 마력
 
   Wand({
-    required this.name,
-    required this.power,
-  });
+    required name,
+    required power,
+  })  : _name = name,
+        _power = power {
+    if (_name.length < 3) {
+      throw Exception('wand의 이름을 3글자 이상 작성해주세요');
+    }
+    if (power < 0.5 || power > 100.0) {
+      throw Exception('Exception 마력은 0.5~100.0 사이여야 합니다.');
+    }
+  }
 }
-
--- lib/23_12_04/game/wizard.dart
+++ lib/23_12_04/game/wizard.dart
@@ -1,20 +1,41 @@
-import 'hero.dart';
 import 'wand.dart';
 
 class Wizard {
-  String name;
-  int hp;
-  int mp;
-  Wand? wand;
+  String _name;
+  int _hp;
+  int _mp;
+  Wand _wand;
 
   Wizard({
-    required this.name,
-    required this.hp,
-    required this.mp,
-    this.wand,
-  });
-
-  void heal(Hero hero) {
-    hero.hp += 10;
+    required String name,
+    required hp,
+    required mp,
+    required Wand wand,
+  })  : _name = name,
+        _hp = hp,
+        _mp = mp,
+        _wand = wand {
+    if (_name.length < 3) {
+      throw Exception('마법사의 이름을 3글자 이상 작성해주세요');
+    }
+    if (mp < 0) {
+      throw Exception('Exception 마법사의 MP는 0 이상이어야 합니다.');
+    }
   }
+
+  void damage(int damage) {
+    _hp -= damage;
+    if (_hp < 0) {
+      _hp = 0;
+    }
+    print('$_name 는 $damage 만큼 피해를 잆었다 \n남은체력 $_hp');
+  }
+}
+
+void main() {
+  Wand wand = Wand(name: '응원봉', power: 50.0);
+  Wizard wizard = Wizard(name: '카리나', hp: 50, mp: 100, wand: wand);
+
+  wizard.damage(1000);
+
 }
