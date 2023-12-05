dca7402f24ce6eeaaf518ae64b7ff17700597ced
Kim.Yushin <offyushin@naver.com>
2023-12-05 15:57:07 +0900

    포이즌슬라임 처음부터 다시

--- bin/untitled.dart
+++ bin/untitled.dart
@@ -1,5 +1,4 @@
-import 'package:untitled/untitled.dart' as untitled;
 
 void main(List<String> arguments) {
-  print('Hello world: ${untitled.calculate()}!');
+  print('Hello world!');
 }
--- lib/23_12_05/game/poisonslime.dart
+++ lib/23_12_05/game/poisonslime.dart
@@ -3,42 +3,11 @@ import 'slime.dart';
 import 'hero.dart';
 
 class PoisonSlime extends Slime {
-  PoisonSlime(super.suffix, this._attackNumbers);
+  PoisonSlime(super.suffix);
 
-  // bool _isFlying = false;
-  //
-  // bool get isFlying => _isFlying;
-  //
-  // set isFlying(bool value) {
-  //   _isFlying = value;
-  //
-  //   if (value) {
-  //     print('$name 이 날개를 펼쳤다');
-  //   } else {
-  //     print('$name 이 날개를 접었다');
-  //   }
-
-  bool _attackNumbers = false;
-
-  bool get attackNumbers => _attackNumbers;
-
-  set attackNumbers(bool value) {
-    _attackNumbers = value;
-    if (attackNumbers) {
-      print('$suffix 이 날개를 펼쳤다');
-    } else {
-      print('$suffix 이 날개를 접었다');
-    }
-  }
-
-  @override
-  void attack(Hero hero) {
-    super.attack(hero);
-  }
 }
 
+
 void main() {
-   PoisonSlime poisonSlime = PoisonSlime('포이즌슬라임', false);
-   Hero hero1 = Hero(name: '홍길동길동', hp: 100);
-   poisonSlime.attack(hero1);
+
 }
