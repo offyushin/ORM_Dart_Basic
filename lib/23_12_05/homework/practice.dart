abcab47cfc16e28976e49fcb7de6633394f935b4
Kim.Yushin <offyushin@naver.com>
2023-12-05 19:20:59 +0900

    연습문제 12-1~12-4

--- lib/23_12_05/homework/practice.dart
+++ lib/23_12_05/homework/practice.dart
@@ -1,7 +1,8 @@
+// 가
 abstract class Asset {
 
 }
-// 가
+
 abstract interface class Thing {
   double get weight;
   set weight(double value);
@@ -23,9 +24,9 @@ abstract class TangibleAsset extends Asset implements Thing {
   late double weight;
 
   TangibleAsset({
-  required this.name,
-  required this.price,
-  required this.color,
+    required this.name,
+    required this.price,
+    required this.color,
   });
 }
 
@@ -53,30 +54,30 @@ class Book extends TangibleAsset {
   }) : _weight = weight;
 }
 
- class Computer extends TangibleAsset {
-   String makerName;
+class Computer extends TangibleAsset {
+  String makerName;
 
-   @override
-   double _weight;
+  @override
+  double _weight;
 
-   @override
-   double get weight => _weight;
+  @override
+  double get weight => _weight;
 
-   @override
-   set weight(double value) {
-     _weight = value;
-   }
+  @override
+  set weight(double value) {
+    _weight = value;
+  }
 
-   Computer({
-     required super.name,
-     required super.price,
-     required super.color,
-     required this.makerName,
-     required double weight,
-   }) : _weight = weight;
- }
+  Computer({
+    required super.name,
+    required super.price,
+    required super.color,
+    required this.makerName,
+    required double weight,
+  }) : _weight = weight;
+}
 
- void main() {
+void main() {
   Book book = Book(name: '오준석의 플러터 생존코딩', price: 25000, color: 'white', isbn: '9791162244371', weight: 1.0);
   Computer computer = Computer(name: '데스크탑', price: 2000000, color: 'silver', makerName: 'Apple', weight: 5.67);
 
@@ -87,4 +88,4 @@ class Book extends TangibleAsset {
   print(computer.name);
 
 
- }
+}
