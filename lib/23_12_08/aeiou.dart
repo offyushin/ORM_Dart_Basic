9bfeb2ae1a4f412e7d4f7451a2268117a289966b
Kim.Yushin <offyushin@naver.com>
2023-12-08 14:50:17 +0900

    aeiou 실습 1~2

--- lib/23_12_08/aeiou.dart
+++ lib/23_12_08/aeiou.dart
@@ -9,10 +9,20 @@ class Word {
            word.substring(i, i + 1) == 'o' ||
            word.substring(i, i + 1) == 'u';
   }
+  
+  bool isConsonant(int i) {
+    // && 은 and 논리 연산자(logical operation) : 모두 true여야 true를 반환
+    return word.substring(i, i + 1) != 'a' &&
+           word.substring(i, i + 1) != 'e' &&
+           word.substring(i, i + 1) != 'i' &&
+           word.substring(i, i + 1) != 'o' &&
+           word.substring(i, i + 1) != 'u';
+  }
 }
 
 void main() {
   Word word =  Word();
 
-  print(word.isVowel(4));
+  print(word.isVowel(4)); // t
+  print(word.isConsonant(5)); // e
 }
