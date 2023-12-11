70a2f9eb7c67b36e7e21dbbc1209b3aa7f48f0c2
Kim.Yushin <offyushin@naver.com>
2023-12-11 15:44:38 +0900

    예외처리 수정

--- lib/23_12_11/practice1_2.dart
+++ lib/23_12_11/practice1_2.dart
@@ -1,13 +1,15 @@
-
 void main() {
+  int num;
+  // final numString = '10.5';
+  // int num = int.parse(numString);
+
   try {
     final numString = '10.5';
-
-    int num = int.parse(numString);
-
+    num = int.parse(numString);
     print(num);
-  } catch(e) {
-    int e = 0;
-    print('에러가 발생하여 ${e} 으로 처리했습니다');
+  } catch (e) {
+    num = 0;
+    print('${num}');
   }
+  print('${num}');
 }
