b0c1e30303843da0b341a2e44a9f6c89593b81f9
Kim.Yushin <offyushin@naver.com>
2023-12-12 14:06:47 +0900

    23_12_12 수업예제

---
+++ lib/23_12_12/00_brakepoint.dart
@@ -0,0 +1,25 @@
+
+
+
+import '../23_12_05/game/hero.dart';
+
+void main() {
+  final heroes = <Hero>{};
+  final h1 = Hero(name: '슈퍼맨', hp: 100);
+  final h2 = Hero(name: '슈퍼맨', hp: 100);
+
+  heroes.add(h1);
+
+  print('-----------');
+
+  myFunction();
+
+  heroes.remove(h2);
+}
+
+void myFunction() {
+  print('-----------');
+  print('-----------');
+  print('-----------');
+  print('-----------');
+}
---
+++ lib/23_12_12/02_exam_transation.dart
@@ -0,0 +1,60 @@
+import 'package:collection/collection.dart';
+
+class Trader {
+  String name;
+  String city;
+
+  Trader(this.name, this.city);
+}
+
+class Transaction {
+  Trader trader;
+  int year;
+  int value;
+
+  Transaction(this.trader, this.year, this.value);
+}
+
+var transactions = [
+  Transaction(Trader("Brian", "Cambridge"), 2011, 300),
+  Transaction(Trader("Raoul", "Cambridge"), 2012, 1000),
+  Transaction(Trader("Raoul", "Cambridge"), 2011, 400),
+  Transaction(Trader("Mario", "Milan"), 2012, 710),
+  Transaction(Trader("Mario", "Milan"), 2012, 700),
+  Transaction(Trader("Alan", "Cambridge"), 2012, 950),
+];
+
+void main() {
+  // 1. 2011년에 일어난 모든 트랜잭션을 찾아 가격 기준 오름차순으로 정리하여 이름을 나열하시오
+  print(transactions.where((e) => e.year == 2011)
+      .sorted((a, b) => a.value.compareTo(b.value))
+      .map((e) => e.trader.name)
+      .toSet()
+      .toList()
+      .length);
+
+
+
+  final result = transactions
+      .where((e) => e.year == 2011)
+      .toList();
+  // print(transactions.where((e) => e.year == 2011).toList().length);
+
+  // print(transactions.map((e) => e.trader.name).toSet().toList());
+
+
+  final person = Person();
+
+  ((person
+    ..hello())..bye());
+}
+
+class Person {
+  void hello() {
+
+  }
+
+  void bye() {
+
+  }
+}
