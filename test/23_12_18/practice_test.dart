44f9d53433325c3a57f65d8c6fce04e02f10483c
Kim.Yushin <offyushin@naver.com>
2023-12-19 09:21:59 +0900

    23_12_18

--- test/23_12_18/practice_test.dart
+++ test/23_12_18/practice_test.dart
@@ -5,15 +5,15 @@ import 'package:untitled/23_12_18/practice_4.dart';
 
 void main() {
   test('짝수면 true', () {
-    expect(isEven(4), true );
+    expect(isEven(4), true);
   });
 
   test('홀수면 false', () {
-    expect(isEven(5), false );
+    expect(isEven(5), false);
   });
 
   test('가장 큰 값', () {
-    expect(findMax([2,5,1,8,10]), 5 );
+    expect(findMax([2, 5, 1, 8, 10]), 5);
   });
 
   test('문자열 뒤집기', () {
